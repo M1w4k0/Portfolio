5bc06277f96c65a28024e98335ac008aaaf9ed82
Zoé-Marie <zoe.charrier@epitech.eu>
2025-04-20 23:43:55 +0200

    maj pages

---
+++ front/src/pages/About /About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css';
+
+export default function About() {
+  return (
+    <div className="about">
+      <h1>À propos</h1>
+      <p>Bienvenue sur la page À propos.</p>
+    </div>
+  );
+}
---
+++ front/src/pages/Experiences/Experiences.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Experiences.css';
+
+export default function Experiences() {
+  return (
+    <div className="experiences">
+      <h1>Expériences</h1>
+    </div>
+  );
+}
