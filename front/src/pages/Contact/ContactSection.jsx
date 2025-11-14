ada5e18b122afb2482330e0ef7b83dd484a08da1
Zoé <zm.charrier@gmail.com>
2025-11-14 01:43:50 +0100

    test

--- front/src/pages/Contact/ContactSection.jsx
+++ front/src/pages/Contact/ContactSection.jsx
@@ -17,7 +17,7 @@ export default function ContactSection() {
     e.preventDefault();
     setStatus("sending");
     try {
-      const res = await fetch("/api/contact", {
+      const res = await fetch("http://localhost:5050/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...formData, t: startTs }),
